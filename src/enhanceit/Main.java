e2e8960b4f7407d7a62bd52d970f4c6021bc0264
Timur <weazy.exe@gmail.com>
2018-10-29 13:01:52 +0300

    fxml gui 70%

--- src/enhanceit/Main.java
+++ src/enhanceit/Main.java
@@ -1,18 +1,27 @@
 package enhanceit;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
+    @FXML
+    public static ImageView imgView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainstage.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setMinWidth(900);
+        primaryStage.setMinHeight(700);
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
