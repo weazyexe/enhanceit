061860c993f301bad7807915e9afa0f32f365648
Timur <weazy.exe@gmail.com>
2018-11-07 21:55:48 +0300

    new ui

--- src/enhanceit/Controller.java
+++ src/enhanceit/Controller.java
@@ -1,4 +1,51 @@
 package enhanceit;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.embed.swing.SwingNode;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import marvin.gui.MarvinImagePanel;
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Controller {
+
+    @FXML
+    ImageView imgView;
+
+    @FXML
+    Button autoButton;
+
+    @FXML
+    BorderPane borderPane;
+
+    @FXML
+    public void openImage() {
+
+        Image img;
+        MarvinImage mImage = MarvinImageIO.loadImage("C:\\Users\\weazy\\Desktop\\beauty11.jpg");
+//        BufferedImage bimg = mImage.getBufferedImage();
+//        mImage.update();
+//        mImage.updateColorArray();
+        BufferedImage bimg = mImage.getBufferedImageNoAlpha();
+        img = SwingFXUtils.toFXImage(bimg, null);
+        imgView.setImage(img);
+    }
+
+    @FXML
+    public void closeApp() {
+        System.exit(0);
+    }
+
+
 }
--- src/enhanceit/Main.java
+++ src/enhanceit/Main.java
@@ -1,32 +1,77 @@
 package enhanceit;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
+import javafx.embed.swing.SwingNode;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import marvin.gui.MarvinImagePanel;
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class Main extends Application {
 
-    @FXML
-    public static ImageView imgView;
+    private double xOffset;
+    private double yOffset;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainstage.fxml"));
+
+//        final SwingNode node = new SwingNode();
+//        createSwingContent(node);
+//
+//        borderPane = (BorderPane)root.lookup("#borderPane");
+//        borderPane.setCenter(node);
+//        root.setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.4), 10, 0.5, 0.0, 0.0);");
+
+        Scene scene = new Scene(root, 920, 700);
+        scene.setOnMousePressed(event -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
+
+
+
+//        primaryStage.getScene().getRoot().setEffect(new DropShadow());
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Hello World");
-        primaryStage.setMinWidth(900);
+        primaryStage.setMinWidth(920);
         primaryStage.setMinHeight(700);
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void createSwingContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(() -> {
+            MarvinImagePanel imagePanel = new MarvinImagePanel();
+            imagePanel.setMinimumSize(new Dimension(800, 550));
+            imagePanel.setMaximumSize(new Dimension(800, 550));
+            //imagePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            MarvinImage mImage = MarvinImageIO.loadImage("C:\\Users\\weazy\\Desktop\\beauty11.jpg");
+            imagePanel.setImage(mImage);
+
+
+            swingNode.setContent(imagePanel);
+        });
     }
 }
