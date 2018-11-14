2d38372134a783e2ee541ca87254c94b8adf8086
weazy <weazy.exe@gmail.com>
2018-11-14 18:31:33 +0300

    d e s i g n

--- src/enhanceit/Main.java
+++ src/enhanceit/Main.java
@@ -2,14 +2,12 @@ package enhanceit;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingNode;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import marvin.gui.MarvinImagePanel;
 import marvin.image.MarvinImage;
 import marvin.io.MarvinImageIO;
@@ -35,19 +33,7 @@ public class Main extends Application {
 //        root.setStyle("-fx-effect: dropshadow(gaussian, rgba(0, 0, 0, 0.4), 10, 0.5, 0.0, 0.0);");
 
         Scene scene = new Scene(root, 920, 700);
-        scene.setOnMousePressed(event -> {
-            xOffset = primaryStage.getX() - event.getScreenX();
-            yOffset = primaryStage.getY() - event.getScreenY();
-        });
-        scene.setOnMouseDragged(event -> {
-            primaryStage.setX(event.getScreenX() + xOffset);
-            primaryStage.setY(event.getScreenY() + yOffset);
-        });
 
-
-
-//        primaryStage.getScene().getRoot().setEffect(new DropShadow());
-        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Hello World");
         primaryStage.setMinWidth(920);
         primaryStage.setMinHeight(700);
